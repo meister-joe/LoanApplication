f6b0ea05e09abf3d96c94c6c4953e05fe9c59aa1
Joe Manahan <meister.joe@outlook.com>
2017-07-26 12:35:20 +0800

    add loan amount and downpayment calculation and value check

--- app/src/main/java/com/joemanahan/meister/loanapplication/MainActivity.java
+++ app/src/main/java/com/joemanahan/meister/loanapplication/MainActivity.java
@@ -1,6 +1,7 @@
 package com.joemanahan.meister.loanapplication;
 
 import android.content.Intent;
+import android.provider.CalendarContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
@@ -8,6 +9,9 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.Calendar;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
     EditText loanAmount;
@@ -40,5 +44,17 @@ public class MainActivity extends AppCompatActivity {
         {
             monthlyRepay.setText(String.format(dblActualLoanAmnt.toString()));
         }
+
+        Calendar cal=Calendar.getInstance();
+        Calendar cal2= Calendar.getInstance();
+        cal.set(2017,7,26);
+        cal2.set(2017,7,28);
+
+
+        Intent inten=new Intent(Intent.ACTION_INSERT).setData(CalendarContract.Events.CONTENT_URI)
+                .putExtra(CalendarContract.Events.TITLE,"Loan")
+                .putExtra(CalendarContract.Events.EVENT_LOCATION,"CBJ")
+                .putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME,cal.getTimeInMillis())
+                .putExtra(CalendarContract.EXTRA_EVENT_END_TIME,cal2.getTimeInMillis());
     }
 }
